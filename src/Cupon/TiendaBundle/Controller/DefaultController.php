8fcd3947c1ae8fa1b6b0234f7e5d546a16ca7de4
viejalospelos <viejalospelos@gmail.com>
2016-03-15 22:34:28 +0100

    Creada portada de la tienda

--- src/Cupon/TiendaBundle/Controller/DefaultController.php
+++ src/Cupon/TiendaBundle/Controller/DefaultController.php
@@ -6,8 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function portadaAction($ciudad, $tienda)
     {
-        return $this->render('TiendaBundle:Default:index.html.twig', array('name' => $name));
+        $em=$this->getDoctrine()->getManager();
+        
+        $ciudad=$em->getRepository('CiudadBundle:Ciudad')->findOneBySlug($ciudad);
+        
+        $tienda=$em->getRepository('TiendaBundle:Tienda')->findOneBy(array(
+        		'slug'=> $tienda,
+        		'ciudad'=>$ciudad->getId()
+        ));        
+        if (!$tienda){
+        	throw $this->createNotFoundException('No existe esa tienda');
+        }
+        
+        $ofertas=$em->getRepository('TiendaBundle:Tienda')->findUltimasOfertasPublicadas($tienda->getId());
+        
+        $cercanas=$em->getRepository('TiendaBundle:Tienda')->findCercanas(
+        		$tienda->getSlug(),
+        		$tienda->getCiudad()->getSlug()
+        		);
+        
+        return $this->render('TiendaBundle:Default:portada.html.twig', array(
+        		'tienda'=>$tienda,
+        		'ofertas'=>$ofertas,
+        		'cercanas'=>$cercanas
+        ));
     }
 }
